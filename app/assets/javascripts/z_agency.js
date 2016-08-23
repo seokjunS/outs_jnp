727afd15ec8bc1dd1144eb9d8f8c5d798b3885ee
seokjunseo <dane2522@snu.ac.kr>
2016-08-23 22:45:41 +0900

    fix

--- app/assets/javascripts/z_agency.js
+++ app/assets/javascripts/z_agency.js
@@ -42,7 +42,7 @@ $(function() {
   $('a.page-scroll').bind('click', function(event) {
     var $anchor = $(this);
     $('html, body').stop().animate({
-      scrollTop: $($anchor.attr('href')).offset().top
+      scrollTop: $($anchor.attr('href')).offset().top - 40
     }, 1500, 'easeInOutExpo');
     event.preventDefault();
   });
